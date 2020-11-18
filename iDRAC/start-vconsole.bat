31455fd28529c54e1f4b1aa768233651b9ee3841
HalianElf <28244771+HalianElf@users.noreply.github.com>
2020-11-18 13:44:48 -0500

    cd to script folder, add pause

--- iDRAC/start-vconsole.bat
+++ iDRAC/start-vconsole.bat
@@ -1,5 +1,5 @@
 @echo off
-
+cd %~dp0
 set /P drachost="Host: "
 set /p dracuser="Username: "
 set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
@@ -8,3 +8,5 @@ set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecur
 for /f "usebackq delims=" %%p in (`%psCommand%`) do set dracpwd=%%p
 
 .\jre\bin\java -cp avctKVM.jar -Djava.library.path=.\lib com.avocent.idrac.kvm.Main ip=%drachost% kmport=5900 vport=5900 user=%dracuser% passwd=%dracpwd% apcp=1 version=2 vmprivilege=true "helpurl=https://%drachost%:443/help/contents.html"
+
+pause
